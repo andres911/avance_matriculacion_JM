1fd10605e73a6422a2bbfdcbe6493674107a4a34
untalyorly <yorliparraga@gmail.com>
2021-12-09 13:53:58 -0500

    d

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,24 +4,22 @@ const helmet = require("helmet");
 const compression = require("compression");
 require("express-async-errors");
 const { NotFoundMiddleware, ErrorMiddleware } = require("../middlewares");
-module.exports = function ({
-    UsuarioRoutes
-}) {
-    const router = express.Router();
-    const apiRoutes = express.Router();
-    apiRoutes
-        .use(cors())
-        .use(helmet())
-        .use(compression())
-        .use(express.json())
-        .use(express.urlencoded({ extended: true }));
+module.exports = function ({ UsuarioRoutes }) {
+  const router = express.Router();
+  const apiRoutes = express.Router();
+  apiRoutes
+    .use(cors())
+    .use(helmet())
+    .use(compression())
+    .use(express.json())
+    .use(express.urlencoded({ extended: true }));
 
-    //api routes de acuedo a cada service
-    apiRoutes.use('/usuario', UsuarioRoutes);
+  //api routes de acuedo a cada service
+  apiRoutes.use("/usuario", UsuarioRoutes);
 
-    ///route principal /v1/api/
-    router.use("/v1/api", apiRoutes);
-    router.use(NotFoundMiddleware);
-    router.use(ErrorMiddleware);
-    return router;
-}
+  ///route principal /v1/api/
+  router.use("/v1/api", apiRoutes);
+  router.use(NotFoundMiddleware);
+  router.use(ErrorMiddleware);
+  return router;
+};
